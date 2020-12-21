37e454e09c514bf9763a461125da4668ecac9e0b
miaomiao <121382277@qq.com>
2020-12-21 11:21:47 +0800

    框架搭建

---
+++ src/main/java/xyz/xn/BlogHApplication.java
@@ -0,0 +1,13 @@
+package xyz.xn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogHApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/xn/BlogHApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
