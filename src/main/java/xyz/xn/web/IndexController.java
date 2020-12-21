82d7d3cdcea2e53b5ef48f30cc7365f7274ea3e5
miaomiao <121382277@qq.com>
2020-12-21 16:08:09 +0800

    添加错误页面

---
+++ src/main/java/xyz/xn/web/IndexController.java
@@ -0,0 +1,21 @@
+package xyz.xn.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import xyz.xn.NotFoundException;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable  String name) {
+//        int i = 1/0;
+        String blog = null;
+//        if (blog == null) {
+//           throw new NotFoundException("博客找不到");
+//        }
+        System.out.println("----index----");
+        return "index";
+    }
+}
